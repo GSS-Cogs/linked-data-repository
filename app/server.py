6bf414a3eef1ee8d165d716a173ce6da98e12a64
Sebastian Cheung <sebastian.cheung@gsscogs.uk>
2022-02-03 22:52:28 +0000

    Testing Foo

--- app/server.py
+++ app/server.py
@@ -9,7 +9,9 @@ async def home(request):
 
 @app.route("/about")
 async def home(request):
-    return text("About")
+    return text("About Test")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
+
+# 22:25 + 3 + 2 = 22:29 should deployed or 4 mins
