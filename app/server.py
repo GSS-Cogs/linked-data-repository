f129d51b02283f85549efd5418a9668811a9c613
Sebastian Cheung <sebastian.cheung@gsscogs.uk>
2022-02-09 11:32:31 +0000

    Hello World

--- app/server.py
+++ app/server.py
@@ -11,6 +11,10 @@ async def home(request):
 async def home(request):
     return text("About Test")
 
+@app.route("/helloworld")
+async def home(request):
+    return text("Hello World")
+
 @app.route("/info")
 async def home(request):
     return text("Info Test Last")
@@ -19,4 +23,4 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
 
 # 22:25 + 3 + 2 ~= 22:29 should deployed or 4 mins
-# 22:53 + 3 + 2 ~= 22:58
+# 22:57 + 3 + 2 ~= 23:02
