42b4ea2f1bc6fb724bd301274fdff994c7cd9cc2
thomasmastersmethods <tom.masters@gsscogs.uk>
2022-02-11 12:13:35 +0000

    setup env vairables

--- tests/test_auth_decorator.py
+++ tests/test_auth_decorator.py
@@ -1,21 +1,33 @@
+import os
+from unittest import mock
 from sanic import Sanic
 import pytest
 import random
+from app.utils.auth.decorator import decorator
+
 
 
 class TestDecorator:
-    @pytest.mark.asyncio
-    @pytest.yield_fixture
+    @pytest.fixture
+
     def app(self):
         app = Sanic(name=str("_" + str(random.randint(0, 10000))))
         yield app
+
+    @pytest.fixture(autouse=True)
+    def mock_settings_env_vars(self):
+        with mock.patch.dict(os.environ, {"OAUTH_DOMAIN": "www.test.com",
+                                          "OAUTH_CLIENT_ID": "DUMMY_CLIENT_ID",
+                                          "OAUTH_CLIENT_SECRET": "DUMMY_CLIENT_SECRET",
+                                          "OAUTH_REDIRECT_URI": "www.test.com"}):
+            yield
 
     def test_decorator_requires_one_of(self):
         """
         When you decorated an endpoing with one or more
         roles, has auth if user has ONE or more of them.
         """
-        ...
+
 
     def test_decorator_requires_all(self):
         """
