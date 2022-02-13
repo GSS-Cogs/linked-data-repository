42389e05a9fd27cf8aa4f49d2c9491aba702cb13
Sebastian Cheung <sebastian.cheung@gsscogs.uk>
2022-02-13 14:25:46 +0000

    Commit Test

--- app/server.py
+++ app/server.py
@@ -10,6 +10,3 @@ async def home(request):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
-
-# 22:25 + 3 + 2 ~= 22:29 should deployed or 4 mins
-# 22:57 + 3 + 2 ~= 23:02
